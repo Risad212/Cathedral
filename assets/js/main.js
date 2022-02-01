70a59a174f1ad91a93de60e7dfc7ad7451f59b9f
Mohammad Risad <83496132+Risad212@users.noreply.github.com>
2022-02-01 16:23:42 +0600

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
- 
+setInterval(() => {
+    const eventDate = new Date('feb 21, 2022 10:00').getTime()
+    const nowDate = new Date().getTime()
+    const dayGap = eventDate - nowDate
+    const days = Math.floor(dayGap / (1000*60*60*24));
+    const hours = Math.floor(dayGap % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
+    const minutes = Math.floor(dayGap % (1000 * 60 * 60) / (1000*60))
+    const seconds = Math.floor(dayGap % (1000 * 60) / (1000))
+    
+    document.querySelector('.day').innerHTML = days;
+    document.querySelector('.hour').innerHTML = hours;
+    document.querySelector('.minute').innerHTML = minutes;
+    document.querySelector('.second').innerHTML = seconds;
+
+},1000)
